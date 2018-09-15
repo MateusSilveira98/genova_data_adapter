ea8fb0bd3f05a320f154b2d34d642d3abc261247
MATEUS COSTA <mateus.costa33@etec.sp.gov.br>
2018-09-15 12:33:20 -0300

    Mudando de ambiente VIVO => AWS

--- knexfile.js
+++ knexfile.js
@@ -7,18 +7,18 @@ module.exports = {
   //   host: 'localhost',
   //   port: '3306'
   // }
-  // connection: {
-  //   user: 'db_user_genova',
-  //   password: 'ornit0quatro',
-  //   database: 'bolt_genova',
-  //   host: 'genova-database.c8r1aoiyjvll.sa-east-1.rds.amazonaws.com',
-  //   port: '3306'
-  // }
   connection: {
-    user: 'root',
-    password: 'RDS_ornito_Genov@123',
+    user: 'db_user_genova',
+    password: 'ornit0quatro',
     database: 'bolt_genova',
-    host: '192.168.1.6',
+    host: 'genova-database.c8r1aoiyjvll.sa-east-1.rds.amazonaws.com',
     port: '3306'
   }
+  // connection: {
+  //   user: 'root',
+  //   password: 'RDS_ornito_Genov@123',
+  //   database: 'bolt_genova',
+  //   host: '192.168.1.6',
+  //   port: '3306'
+  // }
 }
